d67baab1d83a9a4f3cd0ef87d0e2c880eb5eb1ba
sielus <sielus@PC>
2021-04-15 10:11:57 +0200

    working on fragment logic

--- app/src/main/java/com/bluekittystore/ui/contact/ContactFragment.java
+++ app/src/main/java/com/bluekittystore/ui/contact/ContactFragment.java
@@ -1,4 +1,25 @@
 package com.bluekittystore.ui.contact;
 
-public class ContactFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bluekittystore.R;
+import com.bluekittystore.ui.cart.CartViewModel;
+
+public class ContactFragment extends Fragment {
+    ContactViewModel contactViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        contactViewModel = new ContactViewModel();
+        View root = inflater.inflate(R.layout.c, container, false);
+        return root;
+    }
+
+
 }
